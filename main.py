af7095439e2450df282bf63701998bd58a4fc5d0
Abdulrahman Yasser <abdu.ya74@gmail.com>
2023-05-17 15:59:39 +0300

    working fine, just don't delete a song from the playlist XD .Maybe i will fix that later

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pickle
 
 SAVE_FILE = "D:\_Study_A\my_refresh_music"
+MY_URL = "https://www.youtube.com/playlist?list=PLqU9jKopJpR45-N9Obwy1TdDWikKOBWC_"
+
 
 from my_youtube_handler.my_youtube import my_PlaylistDownloader
 
 if __name__ == '__main__':
+
+    p = my_PlaylistDownloader(MY_URL, SAVE_FILE)
+
     with open('data.pickle', 'rb') as file:
         p : my_PlaylistDownloader = pickle.load(file)
 
